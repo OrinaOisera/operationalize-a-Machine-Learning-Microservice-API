bd5f2a188e20c859df1f3ca1d02949bca385bd80
OrinaOisera <orinamark22@gmail.com>
2020-09-12 12:35:23 +0000

    edit added steps for kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="orinaoisera22/machinelearning"
+
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run --generator=run-pod/v1 --image=$dockerpath machinelearning
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods --all-namespaces
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/machinelearning  8000:80
