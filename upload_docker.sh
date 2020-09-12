3a3448dfdb315beb796a439573004ceb0eb8c434
OrinaOisera <orinamark22@gmail.com>
2020-09-12 12:02:54 +0000

    added upload steps

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath="orinaoisera22/machinelearning"
 # Step 2:  
 # Authenticate & tag
+#docker login -u orinaoisera22 --p Mariko22$$
+docker login --username "orinaoisera22" &&\
+    docker image tag machinelearning $dockerpath
+#docker tag machinelearning $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath 
